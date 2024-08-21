b52abd66f8080aecebb19b3069ad69f28c27d43d
MikVR7 <atmikes123@gmail.com>
2024-08-21 22:57:58 +0200

    Added deploy_aws_s3.js

--- src/scripts/config.example.ts
+++ src/scripts/config.example.ts
@@ -1,7 +1,6 @@
 export const config_example = {
-    AWS_REGION: 'your-aws-region',
-    AWS_S3_BUCKET_NAME: 'your-bucket-name',
-    COGNITO_USER_POOL_ID: 'your-user-pool-id',
-    COGNITO_CLIENT_ID: 'your-client-id',
-    COGNITO_IDENTITY_POOL_ID: 'your-identity-pool-id'
+    AWS_REGION:'your-aws-region',
+    AWS_S3_BUCKET_NAME:'your-bucket-name',
+    AWS_ACCESS_KEY_ID: 'your-access-key-id',
+    AWS_SECRET_ACCESS_KEY: 'your-secreet-access-key'
 };
